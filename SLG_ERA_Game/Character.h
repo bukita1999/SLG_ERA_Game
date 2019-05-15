f692d4fe3b0c5f057a5cd9a5c451c216b8d86a0e
高 子轩 <709460120@qq.com>
2019-05-15 10:25:50 +0800

    加了个简单的人物类

---
+++ SLG_ERA_Game/Character.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Character
+{
+private:string *Name;
+		int Point;
+		char *Item;
+		string *Static;
+public: void Chat(){}
+//¼Ó¸öÅÐ¶Ï
+		bool Time();
+		bool Place();
+		bool HeroStatic();
+};
--- SLG_ERA_Game/SLG_ERA_Game.cpp
+++ SLG_ERA_Game/SLG_ERA_Game.cpp
@@ -4,11 +4,7 @@
 #include "pch.h"
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
-	return 0;
-}
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
